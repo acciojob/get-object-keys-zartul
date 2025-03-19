cd6c3363df1c2d1958eb0c2df72472376ee01d33
zartul <121471819+zartul@users.noreply.github.com>
2025-03-19 22:00:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 //your JS code here. If required.
 const student = {
   name: "John",
-	age:25,
-	city:"New York",
 };
 // const keys = getKeys(myObj);
 function getKeys(obj) {
   return Object.keys(obj);
 }
-const keys = getKeys(myObj);
+const keys = getKeys(obj);
 window.student = student;
 window.getKeys = getKeys;
